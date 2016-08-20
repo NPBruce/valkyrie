8d45228c2c720957808a9899dd53d67aac0966cd
Nathan Parslow <nparslow@gmail.com>
2016-08-20 10:56:43 +1000

    Tiles loaded from quest data

--- Assets/Scripts/QuestData.cs
+++ Assets/Scripts/QuestData.cs
@@ -5,12 +5,15 @@ using System.IO;
 
 public class QuestData
 {
-    List<Tile> tiles;
+    public List<Tile> tiles;
     List<string> files;
+    Game game;
 
-    public QuestData(string path)
+    public QuestData(string path, Game game_set)
     {
         Debug.Log("Loading quest from: \"" + path + "\"");
+
+        game = game_set;
 
         tiles = new List<Tile>();
 
@@ -24,20 +27,60 @@ public class QuestData
 
         foreach (string f in files)
         {
-            d = IniRead.ReadFromIni(path);
-            LoadQuestData(d);
+            d = IniRead.ReadFromIni(f);
+            foreach (KeyValuePair<string, Dictionary<string, string>> section in d.data)
+            {
+                AddData(section.Key, section.Value, Path.GetDirectoryName(f));
+            }
         }
     }
 
-    void LoadQuestData(IniData d)
+    void AddData(string name, Dictionary<string, string> content, string path)
     {
+        if (name.IndexOf(Tile.type) == 0)
+        {
+            Tile c = new Tile(name, content, game);
+            tiles.Add(c);
+        }
     }
 
-    class Tile
+    public class Tile : QuestComponent
     {
-        int x;
-        int y;
-        bool visible = false;
-        TileSideData type;
+        public TileSideData tileType;
+        new public static string type = "Tile";
+        public int rotation = 0;
+
+        public Tile(string name, Dictionary<string, string> data, Game game) : base(name, data)
+        {
+            if (data.ContainsKey("rotation"))
+            {
+                rotation = int.Parse(data["rotation"]);
+            }
+            if (data.ContainsKey("side"))
+            {
+                tileType = game.cd.tileSides[data["side"]];
+            }
+        }
+    }
+
+    public class QuestComponent
+    {
+        public int x = 0;
+        public int y = 0;
+        public static string type = "";
+        public bool visible = false;
+
+        public QuestComponent(string name, Dictionary<string, string> data)
+        {
+            if (data.ContainsKey("xposition"))
+            {
+                x = int.Parse(data["xposition"]);
+            }
+
+            if (data.ContainsKey("yposition"))
+            {
+                y = int.Parse(data["yposition"]);
+            }
+        }
     }
 }
--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -9,7 +9,7 @@ public class test : MonoBehaviour {
 
         Game game = FindObjectOfType<Game>();
 
-        string imagePath = @"file://" + game.cd.tileSides["1A"].image;
+        /*string imagePath = @"file://" + game.cd.tileSides["1A"].image;
 
         UnityEngine.UI.Image image;
         Sprite testSprite;
@@ -24,8 +24,33 @@ public class test : MonoBehaviour {
         tile.transform.parent = canvas.transform;
 
         image = tile.AddComponent<UnityEngine.UI.Image>();
-        testSprite = Sprite.Create(newTex, new Rect(0, 0, 1024, 1024), Vector2.zero, 105);
-        image.sprite = testSprite;
+        testSprite = Sprite.Create(newTex, new Rect(0, 0, 1024, 1024), Vector2.zero, 105);*/
+        //image.sprite = testSprite;
+
+        QuestData qd = new QuestData(Application.dataPath + "/../../valkyrie-quests/roag-intro/quest.ini", game);
+
+        foreach(QuestData.Tile t in qd.tiles)
+        {
+            string imagePath = @"file://" +  t.tileType.image;
+
+            UnityEngine.UI.Image image;
+            Sprite testSprite;
+
+            WWW www = new WWW(imagePath);
+            Texture2D newTex = new Texture2D(256, 256, TextureFormat.DXT5, false);
+            www.LoadImageIntoTexture(newTex);
+
+            GameObject tile = new GameObject("Package");
+
+            Canvas canvas = FindObjectOfType<Canvas>();
+            tile.transform.parent = canvas.transform;
+
+            image = tile.AddComponent<UnityEngine.UI.Image>();
+            testSprite = Sprite.Create(newTex, new Rect(0, 0, 1024, 1024), Vector2.zero, 105);
+            image.sprite = testSprite;
+
+        }
+
     }
 
     // Update is called once per frame
