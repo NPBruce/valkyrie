4732393457316f96791974f51553e9de03ab1a51
Nathan Parslow <nparslow@gmail.com>
2016-08-24 14:34:00 +1000

    no message

--- Assets/Scripts/QuestData.cs
+++ Assets/Scripts/QuestData.cs
@@ -99,6 +99,7 @@ public class QuestData
         // If not known ignore
     }
 
+    // Class for Tile components (use TileSide content data)
     public class Tile : QuestComponent
     {
         public TileSideData tileType;
@@ -107,30 +108,59 @@ public class QuestData
 
         public Tile(string name, Dictionary<string, string> data, Game game) : base(name, data)
         {
+            // Get rotation if specified
             if (data.ContainsKey("rotation"))
             {
                 rotation = int.Parse(data["rotation"]);
             }
+            
+            // Find the tileside that is used
             if (data.ContainsKey("side"))
             {
-                // 'TileSide' prefix is optional
+                // 'TileSide' prefix is optional, test both
                 if (game.cd.tileSides.ContainsKey(data["side"]))
+                {
                     tileType = game.cd.tileSides[data["side"]];
+                }
                 else if (game.cd.tileSides.ContainsKey("TileSide" + data["side"]))
+                {
                     tileType = game.cd.tileSides["TileSide" + data["side"]];
+                }
+                else
+                {
+                    // Fatal if not found
+                    Debug.Log("Error: Failed to located TileSide: " + data["side"] + "in quest component: " + name);
+                    Application.Quit();
+                }
+            }
+            else
+            {
+                // Fatal if missing
+                Debug.Log("Error: No TileSide specified in quest component: " + name);
+                Application.Quit();
             }
 
-
+            // Attempt to load image
             string imagePath = @"file://" + tileType.image;
-
             Sprite tileSprite;
-
-            WWW www = new WWW(imagePath);
-            Texture2D newTex = new Texture2D(256, 256, TextureFormat.DXT5, false);
-            www.LoadImageIntoTexture(newTex);
+            WWW www = null;
+            Texture2D newTex = null;
+            try
+            {
+                www = new WWW(imagePath);
+                newTex = new Texture2D(256, 256, TextureFormat.DXT5, false);
+                www.LoadImageIntoTexture(newTex);
+            }
+            catch (System.Exception)
+            {
+                // Fatal if missing
+                Debug.Log("Error: cannot open image file for TileSide: " + imagePath);
+                Application.Quit();
+            }
 
             GameObject tile = new GameObject(name);
 
+            // Locate board canvas to add tile
             Canvas[] canvii = GameObject.FindObjectsOfType<Canvas>();
             Canvas board = canvii[0];
             foreach(Canvas c in canvii)
@@ -142,21 +172,30 @@ public class QuestData
             }
             tile.transform.parent = board.transform;
 
+            // Add image to object
             image = tile.AddComponent<UnityEngine.UI.Image>();
+            // Create sprite from texture
             tileSprite = Sprite.Create(newTex, new Rect(0, 0, newTex.width, newTex.height), Vector2.zero, 1);
+            // Set to transparent initially
             image.color = new Color(255, 255, 255, 0);
+            // Set image sprite
             image.sprite = tileSprite;
+            // Move to get the top left square corner at 0,0
             tile.transform.Translate(Vector3.right * ((newTex.width / 2) - tileType.left), Space.World);
             tile.transform.Translate(Vector3.down * ((newTex.height / 2) - tileType.top), Space.World);
+            // Move to get the middle of the top left square at 0,0 (squares are 105 units)
             tile.transform.Translate(new Vector3(-(float)0.5, (float)0.5, 0) * 105, Space.World);
+            // Set the size to the image size (images are assumed to be 105px per square)
             image.rectTransform.sizeDelta = new Vector2(newTex.width, newTex.height);
 
+            // Rotate around 0,0 rotation amount
             tile.transform.RotateAround(Vector3.zero, Vector3.forward, rotation);
+            // Move tile into target location (spaces are 105 units, Space.World is needed because tile has been rotated)
             tile.transform.Translate(new Vector3(location.x, location.y, 0) * 105, Space.World);
-            //image.color = Color.white;
         }
     }
 
+    // Doors are like tokens but placed differently and have different defaults
     public class Door : Event
     {
         new public static string type = "Door";
@@ -165,6 +204,7 @@ public class QuestData
 
         public Door(string name, Dictionary<string, string> data, Game game) : base(name, data)
         {
+            // Doors are cancelable because you can select then cancel
             cancelable = true;
 
             if (data.ContainsKey("rotation"))
@@ -172,12 +212,21 @@ public class QuestData
                 rotation = int.Parse(data["rotation"]);
             }
 
+            // Default doors are white, but can be any colour
             int[] colour = { 255, 255, 255 };
+            // color is only supported as a hexadecimal "#RRGGBB" format
             if (data.ContainsKey("color"))
             {
-                colour[0] = System.Convert.ToInt32(data["color"].Substring(1, 2), 16);
-                colour[1] = System.Convert.ToInt32(data["color"].Substring(3, 2), 16);
-                colour[2] = System.Convert.ToInt32(data["color"].Substring(5, 2), 16);
+                if ((data["color"].Length != 7) || (data["color"][0] != '#'))
+                {
+                    Debug.Log("Warning: Door color must be in #RRGGBB format in: " + name);
+                }
+                else
+                {
+                    colour[0] = System.Convert.ToInt32(data["color"].Substring(1, 2), 16);
+                    colour[1] = System.Convert.ToInt32(data["color"].Substring(3, 2), 16);
+                    colour[2] = System.Convert.ToInt32(data["color"].Substring(5, 2), 16);
+                }
             }
 
             if (text.Equals(""))
@@ -186,11 +235,18 @@ public class QuestData
             }
 
             Sprite tileSprite;
-
             Texture2D newTex = Resources.Load("sprites/door") as Texture2D;
+            // Check load worked
+            if (newTex == null)
+            {
+                Debug.Log("Error: Cannot load door image");
+                Application.Quit();
+            }
 
+            // Create object
             gameObject = new GameObject(name);
 
+            // Find the token canvas to add to
             Canvas[] canvii = GameObject.FindObjectsOfType<Canvas>();
             Canvas board = canvii[0];
             foreach (Canvas c in canvii)
@@ -202,28 +258,26 @@ public class QuestData
             }
             gameObject.transform.parent = board.transform;
 
+            // Create the image
             image = gameObject.AddComponent<UnityEngine.UI.Image>();
             tileSprite = Sprite.Create(newTex, new Rect(0, 0, newTex.width, newTex.height), Vector2.zero, 1);
+            // Set door colour
             image.color = new Color(colour[0], colour[1], colour[2], 0);
             image.sprite = tileSprite;
-            //tile.transform.Translate(new Vector3(-(float)0.5, (float)0.5, 0) * 105, Space.World);
-            //tile.transform.Translate(new Vector3(location.x, location.y, 0) * 105, Space.World);
             image.rectTransform.sizeDelta = new Vector2(newTex.width, newTex.height);
-
-            //RectTransform trans = tile.AddComponent<RectTransform>();
-            //trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 90, 20);
-            //trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 500, 50);
-
-
+            // Rotate as required
             gameObject.transform.RotateAround(Vector3.zero, Vector3.forward, rotation);
+            // Move to square (105 units per square)
             gameObject.transform.Translate(new Vector3(-(float)0.5, (float)0.5, 0) * 105, Space.World);
             gameObject.transform.Translate(new Vector3(location.x, location.y, 0) * 105, Space.World);
-            //image.color = Color.white;
+            
+            // Find the token canvas script object and add this as a button
             TokenCanvas tc = GameObject.FindObjectOfType<TokenCanvas>();
             tc.add(this);
         }
     }
 
+    // Tokens are events that are tied to a token placed on the board
     public class Token : Event
     {
         new public static string type = "Token";
@@ -231,8 +285,10 @@ public class QuestData
 
         public Token(string name, Dictionary<string, string> data, Game game) : base(name, data)
         {
+            // Tokens are cancelable because you can select then cancel
             cancelable = true;
 
+            // default token type is search, this is the image asset name
             string typeName = "search-token";
             if (data.ContainsKey("type"))
             {
@@ -240,11 +296,25 @@ public class QuestData
             }
 
             Sprite tileSprite;
-
             Texture2D newTex = Resources.Load("sprites/tokens/" + typeName) as Texture2D;
+            // Check if we can find the token image
+            if (newTex == null)
+            {
+                Debug.Log("Warning: Quest component " + name + " is using missing token type: " + typeName);
+                // Use search token instead
+                newTex = Resources.Load("sprites/tokens/search-token") as Texture2D;
+                // If we still can't load it then fatal error
+                if (newTex == null)
+                {
+                    Debug.Log("Error: Cannot load search token \"sprites/tokens/search-token\"");
+                    Application.Quit();
+                }
+            }
 
+            // Create object
             gameObject = new GameObject(name);
 
+            // Find the token canvas to add to
             Canvas[] canvii = GameObject.FindObjectsOfType<Canvas>();
             Canvas board = canvii[0];
             foreach (Canvas c in canvii)
@@ -256,17 +326,22 @@ public class QuestData
             }
             gameObject.transform.parent = board.transform;
 
+            // Create the image
             image = gameObject.AddComponent<UnityEngine.UI.Image>();
             tileSprite = Sprite.Create(newTex, new Rect(0, 0, newTex.width, newTex.height), Vector2.zero, 1);
             image.color = new Color(255, 255, 255, 0);
             image.sprite = tileSprite;
             image.rectTransform.sizeDelta = new Vector2(newTex.width, newTex.height);
+            // Move to square (105 units per square)
             gameObject.transform.Translate(new Vector3(location.x, location.y, 0) * 105, Space.World);
+
+            // Find the token canvas script object and add this as a button
             TokenCanvas tc = GameObject.FindObjectOfType<TokenCanvas>();
             tc.add(this);
         }
     }
 
+    // Events are used to create dialogs that control the quest
     public class Event : QuestComponent
     {
         new public static string type = "Event";
@@ -280,22 +355,31 @@ public class QuestData
 
         public Event(string name, Dictionary<string, string> data) : base(name, data)
         {
+            // Text to be displayed
             if (data.ContainsKey("text"))
             {
                 text = data["text"];
             }
+
+            // Event to trigger on confirm or success
             if (data.ContainsKey("event"))
             {
                 nextEvent = data["event"];
             }
+
+            // Event to trigger on fail, this property will provide pass/fail buttons
             if (data.ContainsKey("failevent"))
             {
                 failEvent = data["failevent"];
             }
+
+            // alter party gold (currently unused)
             if (data.ContainsKey("gold"))
             {
                 gold = int.Parse(data["gold"]);
             }
+
+            // Display hidden components (space separated list)
             if (data.ContainsKey("add"))
             {
                 addComponents = data["add"].Split(' ');
@@ -304,6 +388,8 @@ public class QuestData
             {
                 addComponents = new string[0];
             }
+
+            // Hide components (space separated list)
             if (data.ContainsKey("remove"))
             {
                 removeComponents = data["remove"].Split(' ');
@@ -327,11 +413,14 @@ public class QuestData
         // image for display
         public UnityEngine.UI.Image image;
 
+        // Construct from ini data
         public QuestComponent(string nameIn, Dictionary<string, string> data)
         {
+            name = nameIn;
+
+            // Default to 0, 0 unless specified
             float x = 0, y = 0;
             bool locGiven = false;
-            name = nameIn;
             if (data.ContainsKey("xposition"))
             {
                 locGiven = true;
@@ -343,12 +432,14 @@ public class QuestData
                 locGiven = true;
                 y = float.Parse(data["yposition"]);
             }
+            // location is null if x and y not specified
             if(locGiven)
             {
                 location = new Vector2(x, y);
             }
         }
 
+        // items are invisible by default, can toggle visibility
         public void setVisible(bool vis)
         {
             if (image == null)
@@ -359,6 +450,7 @@ public class QuestData
                 image.color = new Color(image.color.r, image.color.g, image.color.b, 0);
         }
 
+        // return visibility of image
         public bool getVisible()
         {
             if (image == null)
