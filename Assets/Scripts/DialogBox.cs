e373864e5bd70d1d52999bc7841169f0444b8bc5
Nathan Parslow <nparslow@gmail.com>
2016-08-26 11:43:45 +1000

    no message

--- Assets/Scripts/DialogBox.cs
+++ Assets/Scripts/DialogBox.cs
@@ -1,25 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TextButton {
+public class DialogBox {
 
-    public TextButton(Vector2 location, Vector2 size, string text, UnityEngine.Events.UnityAction call)
+    public DialogBox(Vector2 location, Vector2 size, string text)
     {
-        createButton(location, size, text, call, Color.white);
+        createDialog(location, size, text, Color.white, new Color(0, 0, 0, (float)0.7));
     }
 
-    public TextButton(Vector2 location, Vector2 size, string text, UnityEngine.Events.UnityAction call, Color colour)
+    public DialogBox(Vector2 location, Vector2 size, string text, Color fgColour)
     {
-        createButton(location, size, text, call, colour);
+        createDialog(location, size, text, fgColour, new Color(0, 0, 0, (float)0.7));
     }
 
-    void createButton(Vector2 location, Vector2 size, string text, UnityEngine.Events.UnityAction call, Color colour)
+    public DialogBox(Vector2 location, Vector2 size, string text, Color fgColour, Color bgColour)
+    {
+        createDialog(location, size, text, fgColour, bgColour);
+    }
+
+    void createDialog(Vector2 location, Vector2 size, string text, Color fgColour, Color bgColour)
     {
         // Create a object
-        GameObject button = new GameObject("button" + text);
+        GameObject textObj = new GameObject("text" + text.Substring(0, 10));
         GameObject background = new GameObject("buttonBg" + text);
         // Mark it as dialog
-        button.tag = "dialog";
+        textObj.tag = "dialog";
         background.tag = "dialog";
 
         // Find the UI canvas
@@ -38,27 +43,23 @@ public class TextButton {
         transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, location.y, size.y);
         transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, location.x, size.x);
 
-        button.transform.parent = background.transform;
+        textObj.transform.parent = background.transform;
 
-        RectTransform transBt = button.AddComponent<RectTransform>();
+        RectTransform transBt = textObj.AddComponent<RectTransform>();
         transBt.SetParent(transBg);
 
         transBt.localPosition = Vector2.zero;
         transBt.localScale = transBg.localScale;
         transBt.sizeDelta = transBg.sizeDelta;
 
-        button.AddComponent<CanvasRenderer>();
+        textObj.AddComponent<CanvasRenderer>();
         background.AddComponent<CanvasRenderer>();
 
         UnityEngine.UI.Image uiImage = background.AddComponent<UnityEngine.UI.Image>();
-        uiImage.color = new Color(0, 0, 0, (float)0.7);
+        uiImage.color = bgColour;
 
-        UnityEngine.UI.Button uiButton = background.AddComponent<UnityEngine.UI.Button>();
-        uiButton.interactable = true;
-        uiButton.onClick.AddListener(call);
-
-        UnityEngine.UI.Text uiText = button.AddComponent<UnityEngine.UI.Text>();
-        uiText.color = colour;
+        UnityEngine.UI.Text uiText = textObj.AddComponent<UnityEngine.UI.Text>();
+        uiText.color = fgColour;
         uiText.text = text;
         uiText.alignment = TextAnchor.MiddleCenter;
         uiText.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
--- Assets/Scripts/DialogWindow.cs
+++ Assets/Scripts/DialogWindow.cs
@@ -17,50 +17,23 @@ public class DialogWindow {
 
     public void CreateWindow()
     {
-        // holding object
-        GameObject dialog = new GameObject("dialog");
-        // All things are tagged dialog so we can clean up
-        dialog.tag = "dialog";
-
-        // Find the UI canvas
-        Canvas[] canvii = GameObject.FindObjectsOfType<Canvas>();
-        Canvas canvas = canvii[0];
-        foreach (Canvas c in canvii)
-        {
-            if (c.name.Equals("UICanvas"))
-            {
-                canvas = c;
-            }
-        }
-        dialog.transform.parent = canvas.transform;
-
-        // Position the dialog text
-        RectTransform trans = dialog.AddComponent<RectTransform>();
-        trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 30, 200);
-        trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 400, 500);
-        dialog.AddComponent<CanvasRenderer>();
-
-        // Add the text to the component
-        UnityEngine.UI.Text text = dialog.AddComponent<UnityEngine.UI.Text>();
-        text.color = Color.white;
-        text.text = eventData.text.Replace("\\n", "\n");
-        text.font = Resources.GetBuiltinResource<Font>("Arial.ttf");
+        new DialogBox(new Vector2(300, 30), new Vector2(500, 120), eventData.text.Replace("\\n", "\n"));
 
         // Do we have a cancel button?
         if (eventData.cancelable)
         {
-            new TextButton(new Vector2(400, 150), new Vector2(50, 20), "Cancel", delegate { onCancel(); });
+            new TextButton(new Vector2(400, 170), new Vector2(50, 20), "Cancel", delegate { onCancel(); });
         }
         // If there isn't a fail event we have a confirm button
         if(eventData.failEvent.Equals(""))
         {
-            new TextButton(new Vector2(600, 150), new Vector2(50, 20), "Confirm", delegate { onConfirm(); });
+            new TextButton(new Vector2(600, 170), new Vector2(50, 20), "Confirm", delegate { onConfirm(); });
         }
         // Otherwise we have pass and fail buttons
         else
         {
-            new TextButton(new Vector2(500, 150), new Vector2(50, 20), "Fail", delegate { onFail(); }, Color.red);
-            new TextButton(new Vector2(600, 150), new Vector2(50, 20), "Pass", delegate { onPass(); }, Color.green);
+            new TextButton(new Vector2(500, 170), new Vector2(50, 20), "Fail", delegate { onFail(); }, Color.red);
+            new TextButton(new Vector2(600, 170), new Vector2(50, 20), "Pass", delegate { onPass(); }, Color.green);
         }
     }
 
