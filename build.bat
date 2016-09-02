07c086783178c10a4def6c6caeaa026ac6d71789
Nathan Parslow <nparslow@gmail.com>
2016-09-02 22:02:36 +1000

    Adding build script.

---
+++ build.bat
@@ -0,0 +1,14 @@
+del ..\build\valkyrie.zip
+rmdir /s /q ..\build\batch
+mkdir ..\build\batch
+xcopy /E /Y ..\build\unity ..\build\batch
+copy LICENSE ..\build\batch
+copy NOTICE ..\build\batch
+copy dotnetzip-license.rtf ..\build\batch
+mkdir ..\build\batch\valkyrie_Data\valkyrie-contentpacks
+mkdir ..\build\batch\valkyrie_Data\valkyrie-contentpacks\D2E
+mkdir ..\build\batch\valkyrie_Data\valkyrie-questdata
+mkdir ..\build\batch\valkyrie_Data\valkyrie-questdata\example
+xcopy /E /Y ..\valkyrie-contentpacks\D2E ..\build\batch\valkyrie_Data\valkyrie-contentpacks\D2E
+xcopy /E /Y ..\valkyrie-questdata\example ..\build\batch\valkyrie_Data\valkyrie-questdata\example
+"C:\Program Files\7-Zip\7z.exe" a ..\build\valkyrie.zip ..\build\batch\* -r
