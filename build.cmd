bb96e5520de0ff533595198fcd9a66d53bb219af
Nathan Parslow <nparslow@gmail.com>
2016-09-04 20:03:09 +1000

    build automatically adds version number.

--- build.cmd
+++ build.cmd
@@ -11,4 +11,6 @@ mkdir ..\build\batch\valkyrie_Data\valkyrie-questdata
 mkdir ..\build\batch\valkyrie_Data\valkyrie-questdata\example
 xcopy /E /Y ..\valkyrie-contentpacks\D2E ..\build\batch\valkyrie_Data\valkyrie-contentpacks\D2E
 xcopy /E /Y ..\valkyrie-questdata\example ..\build\batch\valkyrie_Data\valkyrie-questdata\example
-"C:\Program Files\7-Zip\7z.exe" a ..\build\valkyrie.zip ..\build\batch\* -r
+set /p version=<Assets\Resources\version.txt
+
+"C:\Program Files\7-Zip\7z.exe" a ..\build\valkyrie-win-%version%.zip ..\build\batch\* -r
