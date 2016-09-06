a9e5693d3c1e1518950ef2f702524e53fae725b5
Nathan Parslow <nparslow@gmail.com>
2016-09-06 14:43:33 +1000

    Update to 0.3.0

--- build.cmd
+++ build.cmd
@@ -8,10 +8,9 @@ copy LICENSE build\batch
 copy NOTICE build\batch
 copy dotnetzip-license.rtf build\batch
 mkdir build\batch\valkyrie_Data\content
-mkdir build\batch\valkyrie_Data\valkyrie-questdata
-mkdir build\batch\valkyrie_Data\valkyrie-questdata\example
+mkdir build\batch\valkyrie_Data\quests
 xcopy /E /Y content build\batch\valkyrie_Data\content
-xcopy /E /Y ..\valkyrie-questdata\example build\batch\valkyrie_Data\valkyrie-questdata\example
+xcopy /E /Y quests build\batch\valkyrie_Data\quests
 set /p version=<Assets\Resources\version.txt
 
 del build\valkyrie-win-%version%.zip
