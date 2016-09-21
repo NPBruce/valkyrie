50ad39e8509c7f301f88e1d7fc8c13c097a81266
Nathan Parslow <nparslow@gmail.com>
2016-09-21 13:50:28 +1000

    Started on campaign

---
+++ Assets/Scripts/Campaign/Campaign.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+class Campaign
+{
+    // SAVE
+    //heros
+    //flags
+
+
+    // DATA
+    // map path
+    // quests:
+    // Name
+    // Path
+    // map location
+    // flags
+
+}
