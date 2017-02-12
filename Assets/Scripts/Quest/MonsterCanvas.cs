8cbbde2c49df3ff3eea76488fe4ce03c04a22b18
Nathan Parslow <nparslow@gmail.com>
2017-02-12 20:25:21 +1100

    Reworking monster images.

--- Assets/Scripts/Quest/MonsterCanvas.cs
+++ Assets/Scripts/Quest/MonsterCanvas.cs
@@ -5,10 +5,9 @@ using System.Collections.Generic;
 public class MonsterCanvas : MonoBehaviour
 {
 
-    public float offset = 0;
-    public Dictionary<string, MonsterIcon> icons;
-    public Dictionary<string, UnityEngine.UI.Image> iconFrames;
+    public int offset = 0;
     public static float monsterSize = 4;
+    public Dictionary<string, MonsterIcon> icons;
 
     // Call to update list of monsters
     public void UpdateList()
@@ -22,38 +21,13 @@ public class MonsterCanvas : MonoBehaviour
         icons = new Dictionary<string, MonsterIcon>();
 
         Game game = Game.Get();
+        int index = 0;
         foreach (Quest.Monster m in game.quest.monsters)
-            AddMonster(m, game);
+        {
+            icons.Add(m.monsterData.name, new MonsterIcon(m, index++));
+        }
 
         UpdateStatus();
-    }
-
-    void AddMonster(Quest.Monster m, Game game)
-    {
-        Sprite mSprite;
-
-        Texture2D newTex = ContentData.FileToTexture(m.monsterData.image);
-
-        GameObject mImg = new GameObject("monsterImg" + m.monsterData.name);
-        mImg.tag = "monsters";
-
-        mImg.transform.parent = game.uICanvas.transform;
-
-        RectTransform trans = mImg.AddComponent<RectTransform>();
-        trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, (0.25f + offset) * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
-        offset += monsterSize + 0.5f;
-        trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0.25f * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
-        mImg.AddComponent<CanvasRenderer>();
-
-        UnityEngine.UI.Image image = mImg.AddComponent<UnityEngine.UI.Image>();
-        icons.Add(m.monsterData.name, image);
-        mSprite = Sprite.Create(newTex, new Rect(0, 0, newTex.width, newTex.height), Vector2.zero, 1);
-        image.sprite = mSprite;
-        image.rectTransform.sizeDelta = new Vector2(monsterSize * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
-
-        UnityEngine.UI.Button button = mImg.AddComponent<UnityEngine.UI.Button>();
-        button.interactable = true;
-        button.onClick.AddListener(delegate { MonsterDiag(m.monsterData.name); });
     }
 
     public void UpdateStatus()
@@ -61,46 +35,7 @@ public class MonsterCanvas : MonoBehaviour
         Game game = Game.Get();
         foreach (Quest.Monster m in game.quest.monsters)
         {
-            UnityEngine.UI.Image image = icons[m.monsterData.name];
-            if (m.activated && m.unique)
-            {
-                image.color = new Color(0f, 0.3f, 0f, 1);
-            }
-            else if (m.activated)
-            {
-                image.color = new Color((float)0.2, (float)0.2, (float)0.2, 1);
-            }
-            else if (m.unique)
-            {
-                image.color = new Color(0.6f, 1f, 0.6f, 1);
-            }
-            else
-            {
-                image.color = Color.white;
-            }
-        }
-    }
-
-    void MonsterDiag(string name)
-    {
-        // If there are any other dialogs open just finish
-        if (GameObject.FindGameObjectWithTag("dialog") != null)
-            return;
-
-        Game game = Game.Get();
-        foreach (Quest.Monster m in game.quest.monsters)
-        {
-            if (name.Equals(m.monsterData.name))
-            {
-                if (game.gameType.DisplayHeroes())
-                {
-                    new MonsterDialog(m);
-                }
-                else
-                {
-                    new MonsterDialogMoM(m);
-                }
-            }
+            icons[m.monsterData.name].Draw(offset);
         }
     }
 
@@ -109,9 +44,6 @@ public class MonsterCanvas : MonoBehaviour
         Quest.Monster m;
 
         Game game;
-
-        UnityEngine.UI.Image icon;
-        UnityEngine.UI.Image frame;
 
         Sprite iconSprite;
         Sprite frameSprite;
@@ -137,31 +69,56 @@ public class MonsterCanvas : MonoBehaviour
             {
                 return;
             }
-            if (index > offset + 6)
+            if (index > offset + 4)
             {
                 return;
             }
 
             GameObject mImg = new GameObject("monsterImg" + m.monsterData.name);
             mImg.tag = "monsters";
+            GameObject mImgFrame = new GameObject("monsterFrame" + m.monsterData.name);
+            mImgFrame.tag = "monsters";
 
             mImg.transform.parent = game.uICanvas.transform;
+            mImgFrame.transform.parent = game.uICanvas.transform;
 
             RectTransform trans = mImg.AddComponent<RectTransform>();
-            trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, (0.25f + (offset * 4.5f)) * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
+            trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, (3.75f + ((index - offset) * 4.5f)) * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
             trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0.25f * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
             mImg.AddComponent<CanvasRenderer>();
+            //RectTransform trans = mImg.AddComponent<RectTransform>();
+            //trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, (3.75f + ((index - offset) * 4.5f)) * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
+            //trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0.25f * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
+            //mImg.AddComponent<CanvasRenderer>();
 
-            icon = mImg.AddComponent<UnityEngine.UI.Image>();
+            UnityEngine.UI.Image icon = mImg.AddComponent<UnityEngine.UI.Image>();
             icon.sprite = iconSprite;
             icon.rectTransform.sizeDelta = new Vector2(monsterSize * UIScaler.GetPixelsPerUnit(), monsterSize * UIScaler.GetPixelsPerUnit());
 
             UnityEngine.UI.Button button = mImg.AddComponent<UnityEngine.UI.Button>();
             button.interactable = true;
             button.onClick.AddListener(delegate { MonsterDiag(m.monsterData.name); });
+
+            if (m.activated && m.unique)
+            {
+                icon.color = new Color(0f, 0.3f, 0f, 1);
+            }
+            else if (m.activated)
+            {
+                icon.color = new Color((float)0.2, (float)0.2, (float)0.2, 1);
+            }
+            else if (m.unique)
+            {
+                icon.color = new Color(0.6f, 1f, 0.6f, 1);
+            }
+            else
+            {
+                icon.color = Color.white;
+            }
+
         }
 
-        void MonsterDiag(string name)
+        static void MonsterDiag(string name)
         {
             // If there are any other dialogs open just finish
             if (GameObject.FindGameObjectWithTag("dialog") != null)
