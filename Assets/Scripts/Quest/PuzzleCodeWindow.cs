a4a60360cf76e4c0df3d0acc003ff80f0a7686c7
Nathan Parslow <Nathan.Parslow@anca.com>
2017-03-21 12:59:33 +1100

    Adding scroll to code guesses (text won't display correctly).

--- Assets/Scripts/Quest/PuzzleCodeWindow.cs
+++ Assets/Scripts/Quest/PuzzleCodeWindow.cs
@@ -77,6 +77,22 @@ public class PuzzleCodeWindow
         db.textObj.GetComponent<UnityEngine.UI.Text>().fontSize = UIScaler.GetMediumFont();
         db.AddBorder();
 
+        // Guesses window
+        DialogBox db = new DialogBox(new Vector2(UIScaler.GetHCenter(-13.5f), 6.5f), new Vector2(17, 13f), "");
+        db.AddBorder();
+        db.background.AddComponent<UnityEngine.UI.Mask>();
+        UnityEngine.UI.ScrollRect scrollRect = db.background.AddComponent<UnityEngine.UI.ScrollRect>();
+
+        GameObject scrollArea = new GameObject("scroll");
+        RectTransform scrollInnerRect = scrollArea.AddComponent<RectTransform>();
+        scrollArea.transform.parent = db.background.transform;
+        transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0, (2 + (puzzle.guess.Count * 2.5f)) * UIScaler.GetPixelsPerUnit());
+        transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, 17 * UIScaler.GetPixelsPerUnit());
+
+        scrollRect.content = scrollInnerRect;
+        scrollRect.horizontal = false;
+        scrollRect.verticalNormalizedPosition = 0f;
+
         float vPos = 7f;
         foreach (PuzzleCode.CodeGuess g in puzzle.guess)
         {
@@ -85,6 +101,7 @@ public class PuzzleCodeWindow
             {
                 db = new DialogBox(new Vector2(hPos, vPos), new Vector2(2f, 2f), i.ToString());
                 db.textObj.GetComponent<UnityEngine.UI.Text>().fontSize = UIScaler.GetMediumFont();
+                db.background.transform.parent = scrollArea.transform;
                 db.AddBorder();
                 hPos += 2.5f;
             }
@@ -94,6 +111,7 @@ public class PuzzleCodeWindow
             {
                 db = new DialogBox(new Vector2(hPos, vPos), new Vector2(2f, 2f), "");
                 db.textObj.GetComponent<UnityEngine.UI.Text>().fontSize = UIScaler.GetMediumFont();
+                db.background.transform.parent = scrollArea.transform;
                 db.AddBorder();
                 hPos += 2.5f;
             }
@@ -101,6 +119,7 @@ public class PuzzleCodeWindow
             {
                 db = new DialogBox(new Vector2(hPos, vPos), new Vector2(2f, 2f), "");
                 db.textObj.GetComponent<UnityEngine.UI.Text>().fontSize = UIScaler.GetMediumFont();
+                db.background.transform.parent = scrollArea.transform;
                 db.AddBorder();
                 hPos += 2.5f;
             }
