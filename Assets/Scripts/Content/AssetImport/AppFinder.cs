aa24e6be729258a876b1d225f9cac47c69054b81
foskon <foskon@gmail.com>
2017-03-31 10:51:38 +0200

    Extract platform finder.

--- Assets/Scripts/Content/AssetImport/AppFinder.cs
+++ Assets/Scripts/Content/AssetImport/AppFinder.cs
@@ -1,7 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
-using Microsoft.Win32;
-using System.IO;
+﻿using Microsoft.Win32;
 using System;
 using System.Text;
 using Read64bitRegistryFrom32bitApp;
@@ -9,70 +6,85 @@ using Read64bitRegistryFrom32bitApp;
 // Class to find FFG app installed
 abstract public class AppFinder
 {
-    public abstract string AppId();
-    public abstract string Destination();
-    public abstract string DataDirectory();
-    public abstract string Executable();
-    public abstract string RequiredFFGVersion();
-    public abstract string RequiredValkyrieVersion();
-    public string location = "";
-    public string exeLocation;
-    public abstract int ObfuscateKey();
+	public abstract string AppId();
+	public abstract string Destination();
+	public abstract string DataDirectory();
+	public abstract string Executable();
+	public abstract string RequiredFFGVersion();
+	public abstract string RequiredValkyrieVersion();
+	public string location = "";
+	public string exeLocation;
+	public abstract int ObfuscateKey();
+	protected Platform platform;
 
-    public AppFinder()
-    {
-        if (Application.platform == RuntimePlatform.OSXPlayer)
-        {
-            ValkyrieDebug.Log("Attempting to locate AppId " + AppId() + " on MacOS.");
-            System.Diagnostics.ProcessStartInfo processStartInfo;
-            System.Diagnostics.Process process;
+	public AppFinder()
+	{
+		SystemHelper helper = new SystemHelper();
+		platform = helper.getPlatform();
 
-            StringBuilder outputBuilder = new StringBuilder();
+		if (platform == Platform.OSX)
+		{
+			ValkyrieDebug.Log("Attempting to locate AppId " + AppId() + " on MacOS.");
+			System.Diagnostics.ProcessStartInfo processStartInfo;
+			System.Diagnostics.Process process;
 
-            processStartInfo = new System.Diagnostics.ProcessStartInfo();
-            processStartInfo.CreateNoWindow = true;
-            processStartInfo.RedirectStandardOutput = true;
-            processStartInfo.RedirectStandardInput = true;
-            processStartInfo.UseShellExecute = false;
-            processStartInfo.Arguments = "SPApplicationsDataType -xml";
-            processStartInfo.FileName = "system_profiler";
+			StringBuilder outputBuilder = new StringBuilder();
 
-            process = new System.Diagnostics.Process();
-            ValkyrieDebug.Log("Starting system_profiler.");
-            process.StartInfo = processStartInfo;
-            // enable raising events because Process does not raise events by default
-            process.EnableRaisingEvents = true;
-            // attach the event handler for OutputDataReceived before starting the process
-            process.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler
-            (
-                delegate (object sender, System.Diagnostics.DataReceivedEventArgs e)
-                {
-                    // append the new data to the data already read-in
-                    outputBuilder.Append(e.Data);
-                }
-            );
-            // start the process
-            // then begin asynchronously reading the output
-            // then wait for the process to exit
-            // then cancel asynchronously reading the output
-            process.Start();
-            process.BeginOutputReadLine();
-            process.WaitForExit();
-            process.CancelOutputRead();
+			processStartInfo = new System.Diagnostics.ProcessStartInfo();
+			processStartInfo.CreateNoWindow = true;
+			processStartInfo.RedirectStandardOutput = true;
+			processStartInfo.RedirectStandardInput = true;
+			processStartInfo.UseShellExecute = false;
+			processStartInfo.Arguments = "SPApplicationsDataType -xml";
+			processStartInfo.FileName = "system_profiler";
+
+			process = new System.Diagnostics.Process();
+			ValkyrieDebug.Log("Starting system_profiler.");
+			process.StartInfo = processStartInfo;
+			// enable raising events because Process does not raise events by default
+			process.EnableRaisingEvents = true;
+			// attach the event handler for OutputDataReceived before starting the process
+			process.OutputDataReceived += new System.Diagnostics.DataReceivedEventHandler
+			(
+				delegate (object sender, System.Diagnostics.DataReceivedEventArgs e)
+				{
+					// append the new data to the data already read-in
+					outputBuilder.Append(e.Data);
+				}
+			);
+			// start the process
+			// then begin asynchronously reading the output
+			// then wait for the process to exit
+			// then cancel asynchronously reading the output
+			process.Start();
+			process.BeginOutputReadLine();
+			process.WaitForExit();
+			process.CancelOutputRead();
+
 
             
-            string output = outputBuilder.ToString();
+            string[] output = outputBuilder.ToString().Split("\n\r".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            ValkyrieDebug.Log("Number of lines returned: " + output.Length);
+
+            if (output.Length == 1)
+            {
+                ValkyrieDebug.Log(output[0]);
+            }
 
             ValkyrieDebug.Log("Looking for: " + "/" + Executable());
             // Quick hack rather than doing XML properly
-            int foundAt = output.IndexOf("/" + Executable());
-            if (foundAt > 0)
+            foreach (string s in output)
             {
-                ValkyrieDebug.Log("Name Index: " + foundAt);
-                int startPos = output.LastIndexOf("<string>", foundAt) + 8;
-                ValkyrieDebug.Log("Start Index: " + startPos);
-                location = output.Substring(startPos, output.IndexOf("</string>") - startPos).Trim();
-                ValkyrieDebug.Log("Using location: " + location);
+
+                if (s.IndexOf("/" + Executable()) > 0)
+                {
+                    ValkyrieDebug.Log("Found Line: " + s);
+                    location = s.Trim();
+                    // Removing <string> and </string>
+                    location = location.Substring(8, location.Length - 17);
+                    ValkyrieDebug.Log("Using location: " + location);
+                }
             }
             if (location.Length == 0)
             {
@@ -95,22 +107,22 @@ abstract public class AppFinder
             }
         }
 
-        exeLocation += location + "/" + Executable();
-        location += DataDirectory();
-        ValkyrieDebug.Log("Asset location: " + location);
-    }
+		exeLocation += location + "/" + Executable();
+		location += DataDirectory();
+		ValkyrieDebug.Log("Asset location: " + location);
+	}
 
-    // Read version of executable from app
-    // Note: This is usually not updated by FFG and is not used for validity checks
-    public string AppVersion()
-    {
-        string ffgVersion = "";
-        try
-        {
-            System.Diagnostics.FileVersionInfo info = System.Diagnostics.FileVersionInfo.GetVersionInfo(exeLocation);
-            ffgVersion = info.ProductVersion;
-        }
-        catch (System.Exception) { }
-        return ffgVersion;
-    }
+	// Read version of executable from app
+	// Note: This is usually not updated by FFG and is not used for validity checks
+	public string AppVersion()
+	{
+		string ffgVersion = "";
+		try
+		{
+			System.Diagnostics.FileVersionInfo info = System.Diagnostics.FileVersionInfo.GetVersionInfo(exeLocation);
+			ffgVersion = info.ProductVersion;
+		}
+		catch (System.Exception) { }
+		return ffgVersion;
+	}
 }
---
+++ Assets/Scripts/Content/AssetImport/SystemHelper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public enum Platform
+{
+	OSX,
+	WIN
+}
+
+public class SystemHelper
+{
+	private Platform _platform;
+
+	public SystemHelper()
+	{
+		RuntimePlatform platform = Application.platform;
+		if (platform == RuntimePlatform.OSXPlayer || platform == RuntimePlatform.OSXEditor)
+		{
+			_platform = Platform.OSX;
+		}
+		else {
+			_platform = Platform.WIN;
+		}
+	}
+
+	public Platform getPlatform()
+	{
+		return _platform;
+	}
+}
