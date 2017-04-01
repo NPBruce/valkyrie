19c9c3c5da5e33416b92af0a14c0195a02caf19c
Nathan Parslow <nparslow@gmail.com>
2017-04-01 22:14:38 +1100

    Code rearrange.

--- Assets/Scripts/Quest/InvestigatorAttack.cs
+++ Assets/Scripts/Quest/InvestigatorAttack.cs
@@ -83,7 +83,7 @@ public class InvestigatorAttack
             new TextButton(new Vector2(UIScaler.GetHCenter(-6f), 9f), new Vector2(12, 2), "Finished", delegate { Destroyer.Dialog(); });
         }
 
-        DrawMonsterIcon();
+        MonsterDialogMoM.DrawMonster(monster);
 
         MonsterHealth();
     }
@@ -153,49 +153,5 @@ public class InvestigatorAttack
             monster.damage = health;
         }
         Attack();
-    }
-
-    public void DrawMonsterIcon()
-    {
-        Game game = Game.Get();
-
-        Texture2D newTex = ContentData.FileToTexture(monster.monsterData.image);
-        Texture2D dupeTex = Resources.Load("sprites/monster_duplicate_" + monster.duplicate) as Texture2D;
-        Sprite iconSprite = Sprite.Create(newTex, new Rect(0, 0, newTex.width, newTex.height), Vector2.zero, 1);
-        Sprite duplicateSprite = null;
-        if (dupeTex != null)
-        {
-            duplicateSprite = Sprite.Create(dupeTex, new Rect(0, 0, dupeTex.width, dupeTex.height), Vector2.zero, 1);
-        }
-
-        GameObject mImg = new GameObject("monsterImg" + monster.monsterData.name);
-        mImg.tag = "dialog";
-        mImg.transform.parent = game.uICanvas.transform;
-
-        RectTransform trans = mImg.AddComponent<RectTransform>();
-        trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 1f * UIScaler.GetPixelsPerUnit(), 8f * UIScaler.GetPixelsPerUnit());
-        trans.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 1f * UIScaler.GetPixelsPerUnit(), 8f * UIScaler.GetPixelsPerUnit());
-        mImg.AddComponent<CanvasRenderer>();
-
-        UnityEngine.UI.Image icon = mImg.AddComponent<UnityEngine.UI.Image>();
-        icon.sprite = iconSprite;
-        icon.rectTransform.sizeDelta = new Vector2(8f * UIScaler.GetPixelsPerUnit(), 8f * UIScaler.GetPixelsPerUnit());
-
-        UnityEngine.UI.Image iconDupe = null;
-        if (duplicateSprite != null)
-        {
-            GameObject mImgDupe = new GameObject("monsterDupe" + monster.monsterData.name);
-            mImgDupe.tag = "dialog";
-            mImgDupe.transform.parent = game.uICanvas.transform;
-
-            RectTransform dupeFrame = mImgDupe.AddComponent<RectTransform>();
-            dupeFrame.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 5f * UIScaler.GetPixelsPerUnit(), UIScaler.GetPixelsPerUnit() * 4f);
-            dupeFrame.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 5f * UIScaler.GetPixelsPerUnit(), 4f * UIScaler.GetPixelsPerUnit());
-            mImgDupe.AddComponent<CanvasRenderer>();
-
-            iconDupe = mImgDupe.AddComponent<UnityEngine.UI.Image>();
-            iconDupe.sprite = duplicateSprite;
-            iconDupe.rectTransform.sizeDelta = new Vector2(4f * UIScaler.GetPixelsPerUnit(), 4f * UIScaler.GetPixelsPerUnit());
-        }
     }
 }
