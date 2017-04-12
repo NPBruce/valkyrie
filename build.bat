6a63b5746378fe0ac775bf7e72dc148bbc7485de
Nathan Parslow <nparslow@gmail.com>
2017-04-12 21:05:49 +1000

    Improving build script.

--- build.bat
+++ build.bat
@@ -1,3 +1,27 @@
+rem cleanup
+rmdir /s /q build\batch
+rmdir /s /q build\batchMac
+
+rem create base structure
+mkdir build
+mkdir build\batch
+mkdir build\batchMac
+
+rem I clean macos because I don't trust unity
+rmdir /s /q build\macos
+mkdir build\macos
+mkdir build\macos\Valkyrie.app
+
+rem Because reasons
+set Target=
+
+rem Build libraries
+C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe libraries/libraries.sln /nologo /p:Configuration="Release" /p:NoWarn=0108
+
+rem build unity
+"%ProgramFiles%\Unity\Editor\Unity.exe" -batchmode -quit -projectPath %~dp0unity -buildWindowsPlayer ..\build\win\valkyrie.exe
+"%ProgramFiles%\Unity\Editor\Unity.exe" -batchmode -quit -projectPath %~dp0unity -buildOSXPlayer ..\build\macos\Valkyrie.app
+
 rem copy lience to win release
 copy LICENSE build\batch\LICENSE.txt
 copy NOTICE build\batch\NOTICE.txt
@@ -27,7 +51,7 @@ rem copy over macos build
 xcopy /E /Y build\macos build\batchMac
 
 rem read build version
-set /p version=<Assets\Resources\version.txt
+set /p version=<unity\Assets\Resources\version.txt
 
 rem delete previous build
 del build\valkyrie-win-%version%.zip
