84aa8eee8f453f0774d39410c600e6ffc6c8ee20
Nathan Parslow <nparslow@gmail.com>
2017-05-11 11:20:32 +1000

    Random hero sets selection.

--- unity/Assets/Scripts/Quest/EventManager.cs
+++ unity/Assets/Scripts/Quest/EventManager.cs
@@ -441,12 +441,20 @@ public class EventManager
 
             // Find and replace rnd:hero with a hero
             // replaces all occurances with the one hero
-            text = text.Replace("{rnd:hero}", game.quest.GetRandomHero().heroData.name.Translate());
+
+            Quest.Hero h = game.quest.GetRandomHero();
+            if (text.Contains("{rnd:hero"))
+            {
+                List<Quest.Hero> selection = new List<Quest.Hero>();
+                selection.Add(h);
+                game.quest.heroSelection.Add(qEvent.sectionName, selection);
+            }
+            text = text.Replace("{rnd:hero}", h.heroData.name.Translate());
 
             // Random heroes can have custom lookups
             if (text.StartsWith("{rnd:hero:"))
             {
-                HeroData hero = game.quest.GetRandomHero().heroData;
+                HeroData hero = h.heroData;
                 int start = "{rnd:hero:".Length;
                 if (!hero.ContainsTrait("male"))
                 {
