b1b88fa1aa3638b014e0ea11a0dfb62cdb2541c5
NPBruce <nparslow@gmail.com>
2017-05-17 14:24:53 +1000

    Reinstate tools menu

---
+++ unity/Assets/Scripts/QuestEditor/ToolsButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.Content;
+
+// Special class for the Menu button present while in a quest
+public class ToolsButton
+{
+    private StringKey TOOLS = new StringKey("val", "TOOLS");
+
+    public ToolsButton()
+    {
+        Game game = Game.Get();
+        if (!game.editMode) return;
+
+        TextButton tb = new TextButton(new Vector2(UIScalers.GetRight(-10.5f), UIScaler.GetBottom(-2.5f)), new Vector2(5, 2), TOOLS, delegate { EditorTools.Create(); });
+        tb.SetFont(game.gameType.GetHeaderFont());
+        // Untag as dialog so this isn't cleared away
+        tb.ApplyTag(Game.QUESTUI);
+    }
+}
