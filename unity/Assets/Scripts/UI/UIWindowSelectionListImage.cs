6cbe0c527089fa9a42098edfc930d0278567b307
Nathan Parslow <nparslow@gmail.com>
2017-05-25 16:01:20 +1000

    Tile list now images

--- unity/Assets/Scripts/UI/UIWindowSelectionListImage.cs
+++ unity/Assets/Scripts/UI/UIWindowSelectionListImage.cs
@@ -41,6 +41,10 @@ namespace Assets.Scripts.UI
                 {
                     xOffset = DrawItem(item, itemScrollArea.GetScrollTransform(), game.cd.images[item.GetKey()], offset, xOffset);
                 }
+                else if (game.cd.tileSides.ContainsKey(item.GetKey()))
+                {
+                    xOffset = DrawItem(item, itemScrollArea.GetScrollTransform(), game.cd.tileSides[item.GetKey()], offset, xOffset);
+                }
                 else if (File.Exists(Path.GetDirectoryName(game.quest.qd.questPath) + "/" + item.GetKey()))
                 {
                     xOffset = DrawItem(item.GetKey(), item.GetColor(), itemScrollArea.GetScrollTransform(), ContentData.FileToTexture(Path.GetDirectoryName(game.quest.qd.questPath) + "/" + item.GetKey()), offset, xOffset);
@@ -65,12 +69,17 @@ namespace Assets.Scripts.UI
             itemScrollArea.SetScrollSize(offset);
         }
 
-
         protected float DrawItem(SelectionItemTraits item, Transform transform, TokenData token, float offset, float xOffset)
         {
             Vector2 texPos = new Vector2(token.x, token.y);
             Vector2 texSize = new Vector2(token.width, token.height);
             Texture2D tex = ContentData.FileToTexture(token.image, texPos, texSize);
+            return DrawItem(item.GetKey(), item.GetColor(), transform, tex, offset, xOffset);
+        }
+
+        protected float DrawItem(SelectionItemTraits item, Transform transform, TileSideData tile, float offset, float xOffset)
+        {
+            Texture2D tex = ContentData.FileToTexture(tile.image);
             return DrawItem(item.GetKey(), item.GetColor(), transform, tex, offset, xOffset);
         }
 
