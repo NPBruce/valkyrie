c3314c95969446acd9c3bd3cccd6c7af3f8b9e34
NPBruce <nparslow@gmail.com>
2017-05-31 14:48:30 +1000

    Starting obb extract

---
+++ libraries/FFGAppImport/AssetImport/ObbExtract.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+class ObbExtract
+{
+    public static string GetAssets(string obb)
+    {
+        ZipFile zip = ZipFile.Read(obb);
+        zip.ExtractAll(Path.GetTempPath() + "/Valkyrie/Obb");
+        zip.Dispose();
+
+        Dictionary<string, int> splitFiles = new Dictionary<string, int>();
+        foreach (string s in Directory.GetFiles(Path.GetTempPath() + "/Valkyrie/Obb", "*.split*", SearchOption.AllDirectories))
+        {
+            string key = Path.GetDirectoryName(s) + "/" + Path.GetFileNameWithoutExtension(s);
+
+            int count = 0;
+            int.TryParse(Path.GetExtension(s).Substring(6), out count);
+
+            if (splitFiles.ContainsKey(key))
+            {
+                if (splitFiles[key] < count)
+                {
+                    splitFiles[key] = count;
+                }
+            }
+            else
+            {
+                splitFiles.Add(key, count);
+            }
+        }
+
+        foreach (KeyValuePair<string, int> kv in splitFiles)
+        {
+            List<byte> data = new List<byte>();
+            for (int i = 0; i <= kv.Value; i++)
+            {
+                data.AddRange(File.ReadAllBytes(kv.Key + ".Split" + i));
+                File.Delete(kv.Key + ".Split" + i);
+            }
+            File.WriteAllBytes(kv.Key, data.ToArray());
+        }
+
+        return Path.GetTempPath() + "/Valkyrie/Obb";
+    }
+
+    public static void CleanTemp()
+    {
+        Directory.Delete(Path.GetTempPath() + "/Valkyrie/Obb", true);
+    }
+}
