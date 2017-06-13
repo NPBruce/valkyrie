251aee7b2512517fe44512ffbe118d70ddd68a37
NPBruce <nparslow@gmail.com>
2017-06-13 15:27:50 +1000

    Adding function comments.

--- unity/Assets/Scripts/UI/UIElement.cs
+++ unity/Assets/Scripts/UI/UIElement.cs
@@ -3,6 +3,10 @@ using Assets.Scripts.Content;
 
 namespace Assets.Scripts.UI
 {
+    /// <summary>
+    /// UIElement is the base class for drawing UI elements.</summary>
+    /// <remarks>
+    /// UIElements can display solid colors, images and text.  It can also act as a button.</remarks>
     public class UIElement
     {
         // The button itself, unity object
@@ -24,32 +28,53 @@ namespace Assets.Scripts.UI
 
         protected UnityEngine.Events.UnityAction buttonCall;
 
+        /// <summary>
+        /// Construct a UI element with options tag name and parent.</summary>
+        /// <param name="t">Unity tag name for the element, cannot be changed after construction, defaults to "dialog".</param>
+        /// <param name="parent">Parent transform, cannot be changed after construction, defaults to the UI Panel.</param>
         public UIElement(string t = "", Transform parent = null)
         {
             if (t.Length > 0) tag = t;
             CreateBG(parent);
         }
 
+        /// <summary>
+        /// Construct a UI element with parent.</summary>
+        /// <param name="parent">Parent transform, cannot be changed after construction.</param>
         public UIElement(Transform parent)
         {
             CreateBG(parent);
         }
 
+        /// <summary>
+        /// The tranform that should be used for sub elements.</summary>
+        /// <returns>
+        /// The background Transform.</returns>
         public Transform GetTransform()
         {
             return bg.transform;
         }
 
+        /// <summary>
+        /// The base RectTransform for the UI element.</summary>
+        /// <returns>
+        /// The background RectTransform.</returns>
         public RectTransform GetRectTransform()
         {
             return bg.GetComponent<RectTransform>();
         }
 
+        /// <summary>
+        /// The tag for this UI element.</summary>
+        /// <returns>
+        /// The Unity tag as a string.</returns>
         public string GetTag()
         {
             return tag;
         }
 
+        /// <summary>
+        /// Internal method called by constructor to set up base GameObject.</summary>
         protected virtual void CreateBG(Transform parent)
         {
             bg = new GameObject("UIBG");
@@ -61,11 +86,21 @@ namespace Assets.Scripts.UI
             bg.transform.SetParent(parent);
         }
 
+        /// <summary>
+        /// Set a background color.</summary>
+        /// <param name="c">Color to use.</param>
+        /// <remarks>
+        /// Can also be used to change image render color, is reset to white when image is set.</remarks>
         public void SetBGColor(Color c)
         {
             bg.GetComponent<UnityEngine.UI.Image>().color = c;
         }
 
+        /// <summary>
+        /// Set a background image.</summary>
+        /// <param name="texture">Texture2D to use.</param>
+        /// <remarks>
+        /// Will also set the background color to white.</remarks>
         public void SetImage(Texture2D texture)
         {
             if (texture == null) return;
@@ -73,6 +108,11 @@ namespace Assets.Scripts.UI
             bg.GetComponent<UnityEngine.UI.Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero, 1);
         }
 
+        /// <summary>
+        /// Set a background image.</summary>
+        /// <param name="sprite">Sprite to use.</param>
+        /// <remarks>
+        /// Will also set the background color to white.</remarks>
         public void SetImage(Sprite sprite)
         {
             if (sprite == null) return;
@@ -81,11 +121,27 @@ namespace Assets.Scripts.UI
         }
 
 
+        /// <summary>
+        /// Set UI location in UIscaler units.</summary>
+        /// <param name="x">Offset from the left of the screen.</param>
+        /// <param name="y">Offset from the top of the screen.</param>
+        /// <param name="width">Horizontal size.</param>
+        /// <param name="height">Vertical size.</param>
+        /// <remarks>
+        /// Cannot be changed after ***FIXME***.</remarks>
         public void SetLocation(float x, float y, float width, float height)
         {
             SetLocationPixels(UIScaler.GetPixelsPerUnit() * x, UIScaler.GetPixelsPerUnit() * y, UIScaler.GetPixelsPerUnit() * width, UIScaler.GetPixelsPerUnit() * height);
         }
 
+        /// <summary>
+        /// Set UI location in pixels.</summary>
+        /// <param name="x">Offset from the left of the screen.</param>
+        /// <param name="y">Offset from the top of the screen.</param>
+        /// <param name="width">Horizontal size.</param>
+        /// <param name="height">Vertical size.</param>
+        /// <remarks>
+        /// Cannot be changed after ***FIXME***.</remarks>
         public virtual void SetLocationPixels(float x, float y, float width, float height)
         {
             RectTransform transBg = bg.GetComponent<RectTransform>();
@@ -93,6 +149,11 @@ namespace Assets.Scripts.UI
             transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, x, width);
         }
 
+        /// <summary>
+        /// Add button press event to element.</summary>
+        /// <param name="call">Function to call on button press.</param>
+        /// <remarks>
+        /// Adds button properties to background area and text.</remarks>
         public virtual void SetButton(UnityEngine.Events.UnityAction call)
         {
             UnityEngine.UI.Button uiButton = bg.AddComponent<UnityEngine.UI.Button>();
@@ -107,26 +168,45 @@ namespace Assets.Scripts.UI
             buttonCall = call;
         }
 
+        /// <summary>
+        /// Set horizontal text padding.</summary>
+        /// <param name="pad">Padding in UIScaler units.</param>
+        /// <remarks>
+        /// Must be called before SetText.</remarks>
         public void SetTextPadding(float pad)
         {
             textPadding = pad;
         }
 
+        /// <summary>
+        /// Set white element text.</summary>
+        /// <param name="content">StringKey to be translated.</param>
         public void SetText(StringKey content)
         {
             SetText(content.Translate(), Color.white);
         }
 
+        /// <summary>
+        /// Set element text.</summary>
+        /// <param name="content">StringKey to be translated.</param>
+        /// <param name="textColor">Color to display text.</param>
         public void SetText(StringKey content, Color textColor)
         {
             SetText(content.Translate(), textColor);
         }
 
+        /// <summary>
+        /// Set white element text.</summary>
+        /// <param name="content">Text to display.</param>
         public void SetText(string content)
         {
             SetText(content, Color.white);
         }
 
+        /// <summary>
+        /// Set element text.</summary>
+        /// <param name="content">Text to display.</param>
+        /// <param name="textColor">Color to display text.</param>
         public virtual void SetText(string content, Color textColor)
         {
             UnityEngine.UI.Text uiText = null;
@@ -167,38 +247,71 @@ namespace Assets.Scripts.UI
             uiText.text = content;
         }
 
+        /// <summary>
+        /// Set font size.</summary>
+        /// <param name="size">Size in pixels.</param>
+        /// <remarks>
+        /// Must be called after SetText.</remarks>
         public virtual void SetFontSize(int size)
         {
             text.GetComponent<UnityEngine.UI.Text>().fontSize = size;
         }
 
+        /// <summary>
+        /// Set font.</summary>
+        /// <param name="font">Font to use.</param>
+        /// <remarks>
+        /// Must be called after SetText.</remarks>
         public virtual void SetFont(Font font)
         {
             text.GetComponent<UnityEngine.UI.Text>().font = font;
         }
 
+        /// <summary>
+        /// Set text alignment.</summary>
+        /// <param name="align">Text alignment.</param>
+        /// <remarks>
+        /// Must be called after SetText.</remarks>
         public virtual void SetTextAlignment(TextAnchor align)
         {
             text.GetComponent<UnityEngine.UI.Text>().alignment = align;
         }
 
+        /// <summary>
+        /// Get the UIElement display text.</summary>
+        /// <returns>
+        /// The display text as a string or "" if not set.</returns>
         public virtual string GetText()
         {
             if (text == null) return "";
             return text.GetComponent<UnityEngine.UI.Text>().text;
         }
 
+        /// <summary>
+        /// Is there any display text?</summary>
+        /// <returns>
+        /// True if text not set or empty</returns>
         public virtual bool Empty()
         {
             if (text == null) return true;
             return text.GetComponent<UnityEngine.UI.Text>().text.Length == 0;
         }
 
+        /// <summary>
+        /// Get the length of a text string at small size with standard font.</summary>
+        /// <param name="content">Text to translate.</param>
+        /// <returns>
+        /// The size of the text in UIScaler units.</returns>
         public static float GetStringWidth(StringKey content)
         {
             return GetStringWidth(content.Translate());
         }
 
+        /// <summary>
+        /// Get the length of a text string at small size with standard font.</summary>
+        /// <param name="content">Text to measure.</param>
+        /// <returns>
+        /// The size of the text in UIScaler units.</returns>
         public static float GetStringWidth(string content)
         {
             if (textWidthObj == null)
@@ -211,15 +324,27 @@ namespace Assets.Scripts.UI
                 textWidthObj.GetComponent<UnityEngine.UI.Text>().fontSize = UIScaler.GetSmallFont();
             }
             textWidthObj.GetComponent<UnityEngine.UI.Text>().text = content;
-            float width = (textWidthObj.GetComponent<UnityEngine.UI.Text>().preferredWidth / UIScaler.GetPixelsPerUnit()) +(textPaddingDefault * 2);
+            float width = (textWidthObj.GetComponent<UnityEngine.UI.Text>().preferredWidth / UIScaler.GetPixelsPerUnit()) + (textPaddingDefault * 2);
             return width;
         }
 
+        /// <summary>
+        /// Get the height of a text box of fixed width with a text string at small size with standard font and standard padding.</summary>
+        /// <param name="content">Text to translate.</param>
+        /// <param name="width">Width of the text box in UIScaler units.</param>
+        /// <returns>
+        /// The required text box height in UIScaler units.</returns>
         public static float GetStringHeight(StringKey content, float width)
         {
             return GetStringHeight(content.Translate(), width);
         }
 
+        /// <summary>
+        /// Get the height of a text box of fixed width with a text string at small size with standard font and standard padding.</summary>
+        /// <param name="content">Text to measure.</param>
+        /// <param name="width">Width of the text box in UIScaler units.</param>
+        /// <returns>
+        /// The required text box height in UIScaler units.</returns>
         public static float GetStringHeight(string content, float width)
         {
             if (textHeightObj == null)
@@ -236,6 +361,10 @@ namespace Assets.Scripts.UI
             return (textHeightObj.GetComponent<UnityEngine.UI.Text>().preferredHeight / UIScaler.GetPixelsPerUnit()) + (textPaddingDefault * 2);
         }
 
+        /// <summary>
+        /// Has the Unity object been destroyed?</summary>
+        /// <returns>
+        /// If the background Unity object has been destroyed.</returns>
         public bool ObjectDestroyed()
         {
             return bg == null;
