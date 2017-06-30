ac1f1c0acedf9aeb3bba6a0f8afda0650fc172c8
NPBruce <nparslow@gmail.com>
2017-06-30 11:39:28 +1000

    Loading fixes.

--- unity/Assets/Scripts/UI/Screens/LoadingScreen.cs
+++ unity/Assets/Scripts/UI/Screens/LoadingScreen.cs
@@ -28,20 +28,26 @@ namespace Assets.Scripts.UI.Screens
             Destroyer.Dialog();
             Draw(display);
 
+            // Create border
+            UIElement ui = new UIElement();
+            ui.SetLocation(3, 27, UIScaler.GetWidthUnits() - 6, 2);
+            ui.SetBGColor(Color.clear);
+            new UIElementBorder(ui);
+
             // Create an object
             GameObject bar = new GameObject("progress");
             // Mark it as dialog
-            logo.tag = Game.DIALOG;
-            logo.transform.SetParent(Game.Get().uICanvas.transform);
+            bar.tag = Game.DIALOG;
+            bar.transform.SetParent(Game.Get().uICanvas.transform);
 
-            RectTransform transBg = logo.AddComponent<RectTransform>();
-            transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 3 * UIScaler.GetPixelsPerUnit(), (UIScaler.Units() - 6) * UIScaler.GetPixelsPerUnit());
+            RectTransform transBg = bar.AddComponent<RectTransform>();
+            transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 3 * UIScaler.GetPixelsPerUnit(), (UIScaler.GetWidthUnits() - 6) * UIScaler.GetPixelsPerUnit());
             transBg.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 27 * UIScaler.GetPixelsPerUnit(), 2 * UIScaler.GetPixelsPerUnit());
 
             // Create the image
-            UnityEngine.UI.Image uiImage = logo.AddComponent<UnityEngine.UI.Image>();
+            UnityEngine.UI.Image uiImage = bar.AddComponent<UnityEngine.UI.Image>();
             uiImage.color = Color.white;
-            logo.AddComponent<ProgressBar>().SetDownload(download);
+            bar.AddComponent<ProgressBar>().SetDownload(download);
         }
 
         /// <summary>
