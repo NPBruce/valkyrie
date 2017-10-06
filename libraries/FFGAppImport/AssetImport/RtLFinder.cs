02500ae09139f69d5665d3563539a7d67d16c4be
Nathan Parslow <nparslow@gmail.com>
2017-10-06 11:17:10 +1100

    #610 Update import to require 1.4.0

--- libraries/FFGAppImport/AssetImport/RtLFinder.cs
+++ libraries/FFGAppImport/AssetImport/RtLFinder.cs
@@ -12,7 +12,7 @@ namespace FFGAppImport
         // If the installed app isn't this or higher don't import
         override public string RequiredFFGVersion()
         {
-            return "1.3.0";
+            return "1.4.0";
         }
         // Steam app ID
         override public string AppId()
