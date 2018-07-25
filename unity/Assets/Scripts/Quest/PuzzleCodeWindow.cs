3f28122aa62adc156a6fa067e3d7f2b1904f7899
BenRQ <40794717+BenRQ@users.noreply.github.com>
2018-07-25 22:11:40 +0200

    #852 [MoM] Additional improvement on French translation  BenRQ committed
    - Improve french translation for puzzle actions
    - Slightly rework image puzzle to avoid overlap (in all languages) and add a bit more room for 'skill'

--- unity/Assets/Scripts/Quest/PuzzleCodeWindow.cs
+++ unity/Assets/Scripts/Quest/PuzzleCodeWindow.cs
@@ -94,7 +94,7 @@ public class PuzzleCodeWindow
         new UIElementBorder(ui);
 
         ui = new UIElement();
-        ui.SetLocation(UIScaler.GetHCenter(6.5f), 1.5f, 6, 2);
+        ui.SetLocation(UIScaler.GetHCenter(6f), 1.5f, 7, 2);
         ui.SetText(new StringKey("val","X_COLON",CommonStringKeys.SKILL));
         ui.SetFontSize(UIScaler.GetMediumFont());
 
