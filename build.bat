00a3ec022391b0e26ed4d2a5dd6d0b51d3019509
redwolf <redwolf2@gmx.de>
2018-10-24 02:52:13 +0200

    singed build

--- build.bat
+++ build.bat
@@ -1,17 +1,27 @@
+@echo off
+rem read build version
 rem read build version
 set /p version=<unity\Assets\Resources\version.txt
 
-
 rem set steam path
-rem set steampath=D:\Steam\
-set steampath=C:\Program Files %28x86%29\Steam\
-
-IF NOT EXIST %steampath% ( 
+IF "%steampath%"=="" SET steampath=%programfiles(x86)%\Steam
+IF NOT EXIST "%steampath%" ( 
 echo [31m--- ERROR --- Steam path not set : please set Steam path in build.bat [0m
 exit /B
 )
 
+rem set java path
+IF "%JAVA_HOME%"=="" SET JAVA_HOME=%ProgramFiles%\Java\jdk1.8.0_192\bin
+IF NOT EXIST "%JAVA_HOME%" ( 
+echo [31m--- ERROR --- JAVA_HOME path not set : please set Java home path in build.bat [0m
+exit /B
+)
+
+SET PATH=%PATH%;%JAVA_HOME%;%ProgramFiles%\Unity\Editor;%ProgramFiles%\7-Zip;%WinDir%/Microsoft.NET/Framework/v4.0.30319;%ProgramFiles(x86)%\NSIS
+@echo on
+
 rem cleanup
+rmdir /s /q build\android
 rmdir /s /q build\batch
 rmdir /s /q build\batchMac
 rmdir /s /q build\batchLinux
@@ -42,20 +52,25 @@ rem Because reasons
 set Target=
 
 rem Build libraries
-C:/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe libraries/libraries.sln /nologo /p:Configuration="Release" /p:NoWarn=0108
+msbuild libraries/libraries.sln /nologo /p:Configuration="Release" /p:NoWarn=0108
 
 rem build unity
-"%ProgramFiles%\Unity\Editor\Unity.exe" -batchmode -quit -projectPath %~dp0unity -buildWindowsPlayer ..\build\win\valkyrie.exe
-rem copy %USERPROFILE%\AppData\Local\Unity\Editor\Editor.log %USERPROFILE%\AppData\Local\Unity\Editor\Editor_valkyrie-windows.log 
+Unity -batchmode -quit -projectPath "%~dp0unity" -buildWindowsPlayer ..\build\win\valkyrie.exe
+rem copy %LOCALAPPDATA%\Unity\Editor\Editor.log %LOCALAPPDATA%\Unity\Editor\Editor_valkyrie-windows.log 
 
-"%ProgramFiles%\Unity\Editor\Unity.exe" -batchmode -quit -projectPath %~dp0unity -buildTarget OSXUniversal -buildOSXUniversalPlayer ..\build\macos\Valkyrie.app
-rem copy %USERPROFILE%\AppData\Local\Unity\Editor\Editor.log %USERPROFILE%\AppData\Local\Unity\Editor\Editor_valkyrie-macos.log 
+Unity -batchmode -quit -projectPath "%~dp0unity" -buildTarget OSXUniversal -buildOSXUniversalPlayer ..\build\macos\Valkyrie.app
+rem copy %LOCALAPPDATA%\Unity\Editor\Editor.log %LOCALAPPDATA%\Unity\Editor\Editor_valkyrie-macos.log 
 
-"%ProgramFiles%\Unity\Editor\Unity.exe" -batchmode -quit -projectPath %~dp0unity -buildLinuxUniversalPlayer ..\build\linux\valkyrie
-rem copy %USERPROFILE%\\AppData\Local\Unity\Editor\Editor.log %USERPROFILE%\AppData\Local\Unity\Editor\Editor_valkyrie-linux.log 
+Unity -batchmode -quit -projectPath "%~dp0unity" -buildLinuxUniversalPlayer ..\build\linux\valkyrie
+rem copy %LOCALAPPDATA%\Unity\Editor\Editor.log %LOCALAPPDATA%\Unity\Editor\Editor_valkyrie-linux.log 
 
-"%ProgramFiles%\Unity\Editor\Unity.exe" -batchmode -quit -projectPath %~dp0unity -executeMethod PerformBuild.CommandLineBuildAndroid +buildlocation ..\build\Valkyrie-android-%version%.apk
-rem copy %USERPROFILE%\AppData\Local\Unity\Editor\Editor.log %USERPROFILE%\AppData\Local\Unity\Editor\Editor_valkyrie-android.log 
+Unity -batchmode -quit -projectPath "%~dp0unity" -executeMethod PerformBuild.CommandLineBuildAndroid +buildlocation "%~dp0build\android\Valkyrie-android.apk"
+rem copy %LOCALAPPDATA%\Unity\Editor\Editor.log %LOCALAPPDATA%\Unity\Editor\Editor_valkyrie-android.log
+
+rem delete the META-INF from the apk
+7z -tzip d "%~dp0build\android\Valkyrie-android.apk" META-INF
+rem jarsigner comes form the JDK
+jarsigner -verbose -keystore "%~dp0unity\user.keystore" -digestalg SHA1 -sigalg MD5withRSA -storepass valkyrie -signedjar "%~dp0build\Valkyrie-android-%version%.apk" "%~dp0build\android\Valkyrie-android.apk" com.bruce.valkyrie
 
 rem copy lience to win release
 copy LICENSE build\batch\LICENSE.txt
@@ -81,30 +96,23 @@ del build\valkyrie-macos-%version%.zip
 del build\valkyrie-linux-%version%.tar.gz
 
 rem create windows zip
-cd build\batch
-"%ProgramFiles%\7-Zip\7z.exe" a ..\valkyrie-windows-%version%.7z * -r
+7z a "%~dp0build\valkyrie-windows-%version%.7z" "%~dp0build\batch\*" -r
 rem create windows 7z
-cd build\batch
-"%ProgramFiles%\7-Zip\7z.exe" a ..\valkyrie-windows-%version%.zip * -r
+7z a "%~dp0build\valkyrie-windows-%version%.zip" "%~dp0build\batch\*" -r
 rem create macos zip
-cd ..\batchMac
-"%ProgramFiles%\7-Zip\7z.exe" a ..\valkyrie-macos-%version%.zip * -r
+7z a "%~dp0build\valkyrie-macos-%version%.zip" "%~dp0build\batchMac\*" -r
 rem create linux tar ball
-cd ..\batchLinux
-"%ProgramFiles%\7-Zip\7z.exe" a ..\valkyrie-linux-%version%.tar * -r
-cd ..
-"%ProgramFiles%\7-Zip\7z.exe" a valkyrie-linux-%version%.tar.gz valkyrie-linux-%version%.tar
-del valkyrie-linux-%version%.tar
+7z a "%~dp0build\batchLinux\valkyrie-linux-%version%.tar" "%~dp0build\batchLinux\*" -r
+7z a "%~dp0build\valkyrie-linux-%version%.tar.gz" "%~dp0build\batchLinux\valkyrie-linux-%version%.tar"
+del "%~dp0build\batchLinux\valkyrie-linux-%version%.tar"
 rem move apk
-move android\test.apk valkyrie-android-%version%.apk
-
-cd ..
+IF EXIST android\test.apk move android\test.apk valkyrie-android-%version%.apk
 
 set /a num=%version:~-1% 2>nul
 
 if "%num%"=="%version:~-1%" (
-    "%ProgramFiles(x86)%\NSIS\makensis" /DVERSION=%version% valkyrie.nsi
+    makensis /DVERSION=%version% valkyrie.nsi
     goto :EOF
 )
 
-"%ProgramFiles(x86)%\NSIS\makensis" /DVERSION=%version% /DPRERELEASE valkyrie.nsi
+makensis /DVERSION=%version% /DPRERELEASE valkyrie.nsi
