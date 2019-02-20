d8d896fd6ebe4f0f5ddd3c3e9136b5679b717f9a
BenRQ <40794717+BenRQ@users.noreply.github.com>
2019-02-20 11:29:45 +0100

    #1067 Lack of max-length of inputs in Editor
    Fine tune text size for Descent and MoM
    Set a max length for quest name in scenario explorer

--- unity/Assets/Scripts/UI/Screens/QuestSelectionScreen.cs
+++ unity/Assets/Scripts/UI/Screens/QuestSelectionScreen.cs
@@ -1001,9 +1001,14 @@ namespace Assets.Scripts.UI.Screens
                 ui.SetBGColor(Color.clear);
                 ui.SetLocation(5.5f, offset + 0.3f, UIScaler.GetWidthUnits() - 8, 1.5f);
                 ui.SetTextPadding(0.5f);
+                if (name_translation.Length >= 70)
+                    name_translation = name_translation.Substring(0, 65) + "(...)";
                 ui.SetText(name_translation, Color.black);
                 ui.SetTextAlignment(TextAnchor.MiddleLeft);
-                ui.SetFontSize(Mathf.RoundToInt(UIScaler.GetSmallFont() * 1.4f));
+                if (game.gameType.TypeName() == "MoM")
+                    ui.SetFontSize(Mathf.RoundToInt(UIScaler.GetSmallFont() * 1.4f));
+                if (game.gameType.TypeName() == "D2E")
+                    ui.SetFontSize(Mathf.RoundToInt(UIScaler.GetSmallFont() * 1.28f));
                 ui.SetFont(game.gameType.GetHeaderFont());
 
                 // Required expansions
@@ -1068,7 +1073,10 @@ namespace Assets.Scripts.UI.Screens
                         synopsys_translation=synopsys_translation.Substring(0, 100) + "(...)";
                     ui.SetText(synopsys_translation, dark_grey_text_color);
                     ui.SetTextAlignment(TextAnchor.MiddleLeft);
-                    ui.SetFontSize(Mathf.RoundToInt(UIScaler.GetSmallFont() * 0.85f));
+                    if (game.gameType.TypeName() == "MoM")
+                        ui.SetFontSize(Mathf.RoundToInt(UIScaler.GetSmallFont() * 0.87f));
+                    if (game.gameType.TypeName() == "D2E")
+                        ui.SetFontSize(Mathf.RoundToInt(UIScaler.GetSmallFont() * 0.80f));
                     ui.SetFontStyle(FontStyle.Italic);
                     ui.SetFont(game.gameType.GetHeaderFont());
                 }
