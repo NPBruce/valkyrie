40fc0f2706dcc46e47090874ed5b536deb423672
Quantumrunner <58113888+Quantumrunner@users.noreply.github.com>
2025-11-16 20:51:09 +0100

    Changed resolution manager file location.

--- unity/Assets/Scripts/Content/ResolutionManager.cs
+++ unity/Assets/Scripts/Content/ResolutionManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Assets.Scripts.Content
+namespace Assets.Scripts
 {
     internal static class ResolutionManager
     {
