7d97d8d49545a6753b49153c3d03476ac609a656
Quantumrunner <58113888+Quantumrunner@users.noreply.github.com>
2025-11-30 10:53:47 +0100

    Improved build.bat logging

--- build.bat
+++ build.bat
@@ -2,11 +2,20 @@
 rem read build version
 set /p version=<unity\Assets\Resources\version.txt
 
+echo [%TIME%] --- Starting Build Process ---
+echo [%TIME%] Version: %version%
+
 rem set default build flags if not set
 IF "%BUILD_WINDOWS%"=="" SET BUILD_WINDOWS=true
 IF "%BUILD_MAC%"=="" SET BUILD_MAC=true
 IF "%BUILD_LINUX%"=="" SET BUILD_LINUX=true
 IF "%BUILD_ANDROID%"=="" SET BUILD_ANDROID=true
+
+echo [%TIME%] --- Build Configuration ---
+echo [%TIME%] BUILD_WINDOWS: %BUILD_WINDOWS%
+echo [%TIME%] BUILD_MAC:     %BUILD_MAC%
+echo [%TIME%] BUILD_LINUX:   %BUILD_LINUX%
+echo [%TIME%] BUILD_ANDROID: %BUILD_ANDROID%
 
 rem set steam path. You can get it from https://store.steampowered.com/
 rem is steam needed for building?
@@ -19,6 +28,7 @@ exit /B 1
 :comment
 
 if "%BUILD_ANDROID%"=="true" (
+    echo [%TIME%] Checking Android Environment...
     rem set open java development kit path. You can get it from https://developers.redhat.com/products/openjdk/download/
     IF "%JDK_HOME%"=="" SET JDK_HOME=%JAVA_HOME%
     IF "%JDK_HOME%"=="" SET JDK_HOME=%ProgramFiles%\RedHat\java-1.8.0-openjdk-1.8.0.212-3
@@ -40,6 +50,7 @@ if "%BUILD_ANDROID%"=="true" (
     echo [31m--- ERROR --- ANDROID_BUILD_TOOLS path not set : please set android build tool path in build.bat or create a similar environment variable[0m
     exit /B 1
     )
+    echo [%TIME%] Android Environment OK.
 )
 
 rem set unity editor location
@@ -53,12 +64,13 @@ rem find visual studio installation path so we can find the msbuild executable. 
 rem this way we can find whatever year/version of vs is installed.
 FOR /D %%B in ("%ProgramFiles%\Microsoft Visual Studio\*") do (SET "VSPATH=%%B")
 
-echo using visual studio path: %VSPATH%
+echo [%TIME%] Using Visual Studio path: %VSPATH%
 
 rem you can get NSIS from https://nsis.sourceforge.io/Main_Page
 SET PATH=%PATH%;%JDK_HOME%\bin;%UNITY_EDITOR_HOME%;%ProgramFiles%\7-Zip;%VSPATH%\Community\MSBuild\Current\Bin\;%ProgramFiles(x86)%\NSIS;%~dp0libraries\SetVersion\bin\Release;%ANDROID_BUILD_TOOLS%;%localappdata%\NuGet;%ProgramFiles%\Git\bin
 
 rem cleanup
+echo [%TIME%] Cleaning up previous builds...
 rmdir /s /q build\android
 rmdir /s /q build\batch
 rmdir /s /q build\batchMac
@@ -69,6 +81,7 @@ rmdir /s /q build\linux
 del build\Valkyrie-android-%version%.apk
 
 rem create base structure
+echo [%TIME%] Creating build directories...
 mkdir build
 mkdir build\batch
 mkdir build\batchMac
@@ -87,58 +100,79 @@ mkdir build\macos\Valkyrie.app
 mkdir build\linux
 
 rem download the latest version of Android Storage Access Framework library.
+echo [%TIME%] Downloading Android dependencies...
 POWERSHELL -Command "Invoke-WebRequest -Uri https://github.com/seinsinnes/FSAF/releases/latest/download/fsaf-release.aar -OutFile .\unity\Assets\Plugins\Android\fsaf-release.aar"
 
 rem Because reasons
 set Target=
 
 rem install any external packages required by the libraries
+echo [%TIME%] Restoring NuGet packages...
 winget install -q Microsoft.NuGet -l "%localappdata%\NuGet" --accept-source-agreements --accept-package-agreements
 nuget restore libraries/libraries.sln
 
 rem Build libraries
+echo [%TIME%] Building libraries...
 msbuild libraries/libraries.sln /nologo /p:Configuration="Release" /p:NoWarn=0108
+echo [%TIME%] Libraries built.
 
 rem this will change the version information in the project settings
+echo [%TIME%] Updating version info...
 SetVersion %~dp0
 
 del ".\unity\Assets\Plugins\UnityEngine.dll"
 
 rem build unity
+echo [%TIME%] --- Starting Unity Builds ---
+
 if "%BUILD_WINDOWS%"=="true" (
+    echo [%TIME%] Building for Windows...
     Unity -batchmode -quit -projectPath "%~dp0unity" -buildWindowsPlayer ..\build\win\valkyrie.exe
     copy %LOCALAPPDATA%\Unity\Editor\Editor.log .\build\Editor_valkyrie-windows.log
+    echo [%TIME%] Windows build complete. Log: .\build\Editor_valkyrie-windows.log
 )
 
 if "%BUILD_MAC%"=="true" (
+    echo [%TIME%] Building for macOS...
     Unity -batchmode -quit -projectPath "%~dp0unity" -buildTarget OSXUniversal -buildOSXUniversalPlayer ..\build\macos\Valkyrie.app
     copy %LOCALAPPDATA%\Unity\Editor\Editor.log .\build\Editor_valkyrie-macos.log
+    echo [%TIME%] macOS build complete. Log: .\build\Editor_valkyrie-macos.log
 )
 
 if "%BUILD_LINUX%"=="true" (
+    echo [%TIME%] Building for Linux...
     Unity -batchmode -quit -projectPath "%~dp0unity" -buildLinux64Player ..\build\linux\valkyrie
     copy %LOCALAPPDATA%\Unity\Editor\Editor.log .\build\Editor_valkyrie-linux.log
+    echo [%TIME%] Linux build complete. Log: .\build\Editor_valkyrie-linux.log
 )
 
 if "%BUILD_ANDROID%"=="true" (
+    echo [%TIME%] Building for Android...
     Unity -batchmode -quit -projectPath "%~dp0unity" -executeMethod PerformBuild.CommandLineBuildAndroid +buildlocation "%~dp0build\android\Valkyrie-android.apk"
     set BUILD_STATUS=%ERRORLEVEL%
     copy %LOCALAPPDATA%\Unity\Editor\Editor.log .\build\Editor_valkyrie-android.log
+    echo [%TIME%] Android build finished with status: %BUILD_STATUS%. Log: .\build\Editor_valkyrie-android.log
+    
     if %BUILD_STATUS% NEQ 0 (
-        echo "Unity Build Failed"
+        echo [31m--- ERROR --- Unity Build Failed[0m
         exit /b 1
     )
 
     rem delete the META-INF from the apk
+    echo [%TIME%] Processing APK...
     7z -tzip d "%~dp0build\android\Valkyrie-android.apk" META-INF
     rem jarsigner comes form the JDK
+    echo [%TIME%] Signing APK...
     jarsigner -keystore "%~dp0unity\user.keystore" -storepass valkyrie -keypass valkyrie "%~dp0build\android\Valkyrie-android.apk" com.bruce.valkyrie
     jarsigner -verify -verbose -certs "%~dp0build\android\Valkyrie-android.apk"
     rem zipalign comes from the android build tools
+    echo [%TIME%] Aligning APK...
     zipalign -v 4 "%~dp0build\android\Valkyrie-android.apk" "%~dp0build\Valkyrie-android-%version%.apk"
+    echo [%TIME%] Android post-processing complete.
 )
 
 rem copy lience to win release
+echo [%TIME%] Copying licenses...
 copy LICENSE build\batch\LICENSE.txt
 copy NOTICE build\batch\NOTICE.txt
 copy .NET-Ogg-Vorbis-Encoder-LICENSE build\batch\.NET-Ogg-Vorbis-Encoder-LICENSE.txt
@@ -148,13 +182,17 @@ xcopy /I /E /Y build\batch build\batchMac\Valkyrie.app
 xcopy /I /E /Y build\batch build\batchLinux\valkyrie-linux-%version%
 
 rem copy over windows build
+echo [%TIME%] Packaging Windows build...
 xcopy /E /Y build\win build\batch
 rem copy over macos build
+echo [%TIME%] Packaging macOS build...
 xcopy /E /Y build\macos build\batchMac
 rem copy over linux build
+echo [%TIME%] Packaging Linux build...
 xcopy /E /Y build\linux build\batchLinux\valkyrie-linux-%version%
 
 rem delete previous build
+echo [%TIME%] Removing old packages...
 del build\valkyrie-windows-%version%.exe
 del build\valkyrie-windows-%version%.zip
 del build\valkyrie-windows-%version%.7z
@@ -163,18 +201,21 @@ del build\valkyrie-linux-%version%.tar.gz
 
 rem create windows zip
 if "%BUILD_WINDOWS%"=="true" (
+    echo [%TIME%] Zipping Windows build...
     7z a "%~dp0build\valkyrie-windows-%version%.7z" "%~dp0build\batch\*" -r
     rem create windows 7z
     7z a "%~dp0build\valkyrie-windows-%version%.zip" "%~dp0build\batch\*" -r
 )
 rem create macos tar ball
 if "%BUILD_MAC%"=="true" (
+    echo [%TIME%] Compressing macOS build...
     7z a "%~dp0build\batchMac\valkyrie-macos-%version%.tar" "%~dp0build\batchMac\*" -r
     7z a "%~dp0build\valkyrie-macos-%version%.tar.gz" "%~dp0build\batchMac\valkyrie-macos-%version%.tar"
     del "%~dp0build\batchMac\valkyrie-macos-%version%.tar"
 )
 rem create linux tar ball
 if "%BUILD_LINUX%"=="true" (
+    echo [%TIME%] Compressing Linux build...
     7z a "%~dp0build\batchLinux\valkyrie-linux-%version%.tar" "%~dp0build\batchLinux\*" -r
     7z a "%~dp0build\valkyrie-linux-%version%.tar.gz" "%~dp0build\batchLinux\valkyrie-linux-%version%.tar"
     del "%~dp0build\batchLinux\valkyrie-linux-%version%.tar"
@@ -184,9 +225,13 @@ IF EXIST android\test.apk move android\test.apk valkyrie-android-%version%.apk
 
 set /a num=%version:~-1% 2>nul
 
+echo [%TIME%] Creating Installer...
 if "%num%"=="%version:~-1%" (
     makensis /DVERSION=%version% valkyrie.nsi
+    echo [%TIME%] Installer created (Release).
     goto :EOF
 )
 
 makensis /DVERSION=%version% /DPRERELEASE valkyrie.nsi
+echo [%TIME%] Installer created (Pre-release).
+echo [%TIME%] --- Build Process Complete ---
